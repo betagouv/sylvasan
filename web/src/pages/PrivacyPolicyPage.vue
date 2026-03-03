97c47f1b1d1c3fb319545d3c44377cab5a6547a7
Alejandro MG <alejandro@amguillen.dev>
2026-03-03 13:40:38 +0100

    Ajout de la page des données personnelles

---
+++ web/src/pages/PrivacyPolicyPage.vue
@@ -0,0 +1,277 @@
+<route lang="json">
+{
+  "path": "/donnees-personnelles",
+  "meta": {
+    "title": "Données Personnelles"
+  }
+}
+</route>
+
+<script setup lang="ts"></script>
+
+<template>
+  <div class="fr-container mb-10">
+    <DsfrBreadcrumb
+      :links="[{ to: '/', text: 'Accueil' }, { text: 'Données personnelles' }]"
+    />
+    <h1>Politique de confidentialité</h1>
+    <h2>Traitement des données à caractère personnel</h2>
+    <p>
+      La présente plateforme SylvaSan est à l’initiative du Ministère de
+      l’Agriculture et de la Souveraineté Alimentaire (Département de la Santé
+      des Forêts), qui est responsable de traitement des données à caractère
+      personnel collectées par la plateforme.
+    </p>
+    <h2>Finalités</h2>
+    <p>
+      La plateforme numérique SylvaSan est une application mobile destinée aux
+      forestiers du Département de la santé des forêts qui vise à sécuriser et
+      encourager la signalisation des symptômes en forêt.
+    </p>
+    <h2>Données à caractère personnel traitées</h2>
+    <p>
+      La plateforme peut traiter les données à caractère personnel concernant
+      plusieurs types de profils :
+    </p>
+    <ul>
+      <li>Utilisateur ou utilisatrice non identifié sur la plateforme</li>
+      <li>Utilisateur ou utilisatrice réalisant des signalisations</li>
+      <li>
+        Utilisateur ou utilisatrice réalisant les enquêtes et analysant les
+        données resultantes des signalisations
+      </li>
+    </ul>
+    <p>
+      Pour ces différents types, la plateforme peut traiter les données
+      suivantes :
+    </p>
+    <ul>
+      <li>
+        <span class="font-bold"
+          >Données relatives à la création d’un compte</span
+        >
+        : Certaines données sont obligatoires pour la création du compte : nom,
+        prénom, nom d'utilisateur, adresse e-mail.
+      </li>
+      <li>
+        <span class="font-bold">Données de contact</span> : Certaines données
+        peuvent être collectées lors de la prise de contact avec les différents
+        acteurs, notamment l’adresse-mail et de manière facultative le prénom et
+        le nom de la personne de contact.
+      </li>
+      <li><span class="font-bold">Données d’hébergeur</span>.</li>
+      <li>
+        <span class="font-bold">Cookies</span> : Des cookies sont utilisés dans
+        un objectif d’amélioration de la plateforme.
+      </li>
+    </ul>
+    <h2>Bases juridiques des traitements de données</h2>
+    <p>
+      Les données traitées à l’occasion de ces traitements ont plusieurs
+      fondements juridiques :
+    </p>
+    <ul>
+      <li>
+        Le consentement de la personne concernée pour une ou plusieurs finalités
+        spécifiques au sens de l’article 6-a du RGPD et en application de
+        l’article 5(3) de la directive 2002/58/CE modifiée ;
+      </li>
+      <li>
+        L’exécution d’une mission d’intérêt public ou relevant de l’exercice de
+        l’autorité publique dont est investi le responsable de traitement au
+        sens de l’article 6-e du RPGD ;
+      </li>
+      <li>
+        Une obligation légale à laquelle le responsable de traitement est soumis
+        au sens de l'article 6-c du RGPD.
+      </li>
+    </ul>
+    <p>Ces fondements sont précisés ci-dessous :</p>
+    <h3>a) Données relatives à la création d’un compte</h3>
+    <p>
+      Ce traitement est nécessaire à l’exécution d’une mission d’intérêt public
+      ou relevant de l’exercice de l’autorité publique dont est investi le
+      responsable de traitement au sens de l’article 6-e du règlement (UE)
+      2016/679 du Parlement européen et du Conseil du 27 avril 2016 relatif à la
+      protection des personnes physiques à l’égard du traitement des données à
+      caractère personnel et à la libre circulation de ces données.
+    </p>
+    <h3>b) Données de contact</h3>
+    <p>
+      Ce traitement est nécessaire à l’exécution d’une mission d’intérêt public
+      ou relevant de l’exercice de l’autorité publique dont est investi le
+      responsable de traitement au sens de l’article 6-e du règlement (UE)
+      2016/679 du Parlement européen et du Conseil du 27 avril 2016 relatif à la
+      protection des personnes physiques à l’égard du traitement des données à
+      caractère personnel et à la libre circulation de ces données.
+    </p>
+    <h3>c) Données d’hébergeur ou de connexion</h3>
+    <p>
+      Ce traitement est nécessaire au respect d'une obligation légale à laquelle
+      le responsable de traitement est soumis au sens de l'article 6-c du
+      Règlement (UE) 2016/679 du Parlement européen et du Conseil du 27 avril
+      2016 relatif à la protection des personnes physiques à l'égard du
+      traitement des données à caractère personnel et à la libre circulation de
+      ces données.
+    </p>
+    <p>
+      L'obligation légale est posée par la loi LCEN n° 2004-575 du 21 juin 2004
+      pour la confiance dans l'économie numérique et par l’article 1 du décret
+      n° 2021-1363 du 20 octobre 2021.
+    </p>
+    <h3>d) Cookies</h3>
+    <p>
+      En application de l’article 5(3) de la directive 2002/58/CE modifiée
+      concernant le traitement des données à caractère personnel et la
+      protection de la vie privée dans le secteur des communications
+      électroniques, transposée à l’article 83 de la loi n°78-17 du 6 janvier
+      1978 relative à l’informatique, aux fichiers et aux libertés, les traceurs
+      ou cookies suivent deux régimes distincts.
+    </p>
+    <p>
+      Les cookies strictement nécessaires au service ou n’ayant pas pour
+      finalité exclusive de faciliter la communication par voie électronique
+      sont dispensés de consentement préalable au titre de l’article 83 de la
+      loi n°78-17 du 6 janvier 1978.
+    </p>
+    <p>
+      Les cookies n’étant pas strictement nécessaires au service ou n’ayant pas
+      pour finalité exclusive de faciliter la communication par voie
+      électronique doivent être consenti par l’utilisateur.
+    </p>
+    <p>
+      Ce consentement de la personne concernée pour une ou plusieurs finalités
+      spécifiques constitue une base légale au sens du RGPD et doit être entendu
+      au sens de l'article 6-a du Règlement (UE) 2016/679 du Parlement européen
+      et du Conseil du 27 avril 2016 relatif à la protection des personnes
+      physiques à l'égard du traitement des données à caractère personnel et à
+      la libre circulation de ces données.
+    </p>
+    <p>
+      Les cookies de mesure d’audience, lorsque les données sont anonymisées, ne
+      nécessitent pas de consentement. En l’occurrence, la plateforme utilise
+      Matomo dans sa version anonymisée, vous pouvez néanmoins vous opposer au
+      suivi de votre navigation sur ce site web. Pour plus d’informations,
+      visitez
+      <router-link :to="{ name: '/CookiesPage' }" class="fr-link"
+        >notre page dédiée.</router-link
+      >
+    </p>
+    <h2>Durée de conservation</h2>
+    <DsfrTable
+      title="Durée de conservation"
+      :rows="[
+        [
+          'Données relatives à la création d’un compte',
+          '4 ans à compter de la dernière utilisation du compte.',
+        ],
+        [
+          'Données de contact',
+          '4 ans à compter de la dernière prise de contact.',
+        ],
+        [
+          'Données d’hébergeur',
+          '1 an, conformément au le décret n° 2021-1363 du 20 octobre 2021.',
+        ],
+        [
+          'Cookies',
+          'Dès le retrait du consentement ou dans un délai de 13 mois, conformément aux recommandations de la CNIL',
+        ],
+      ]"
+      no-caption
+      :pagination="false"
+    />
+    <h2>Droit des personnes concernées</h2>
+    <p>
+      Selon la loi n° 78-17 du 6 janvier 1978 relative à l'informatique aux
+      fichiers et aux libertés, vous disposez des droits suivants concernant vos
+      données à caractère personnel :
+    </p>
+    <ul>
+      <li>Droit d’information et droit d’accès aux données ;</li>
+      <li>
+        Droit de rectification et le cas échéant de suppression des données ;
+      </li>
+      <li>
+        Droit au retrait du consentement en matière de cookies uniquement s’il y
+        a des cookies nécessitant le consentement.
+      </li>
+    </ul>
+    <p>
+      Pour les exercer, faites-nous parvenir une demande en précisant la date et
+      l’heure précise de la requête – ces éléments sont indispensables pour nous
+      permettre de retrouver votre recherche – à l’adresse suivante :
+    </p>
+    <p>
+      Par voie numérique :
+      <a :href="`mailto:contact@sylvasan.beta.gouv.fr`" class="fr-link"
+        >contact@sylvasan.beta.gouv.fr</a
+      >
+    </p>
+    <p>Par voie postale :</p>
+    <address>
+      <p>
+        Ministère de l’Agriculture et de la Souveraineté Alimentaire
+        <br />
+        Département de la Santé des Forêts
+        <br />
+        251 rue de Vaugirard 75732 Paris Cedex 15
+      </p>
+    </address>
+    <p>
+      En raison de l’obligation de sécurité et de confidentialité dans le
+      traitement des données à caractère personnel qui incombe au responsable de
+      traitement, votre demande ne sera traitée que si vous apportez la preuve
+      de votre identité. Pour vous aider dans votre démarche,
+      <ExternalLink
+        href="https://www.cnil.fr/fr/modele/courrier/exercer-son-droit-dacces"
+        text="un modèle de courrier élaboré par la Cnil"
+      />
+      est à votre disposition.
+    </p>
+    <p>
+      Vous avez la possibilité de vous opposer à un traitement de vos données
+      personnelles. Pour vous aider dans votre démarche,
+      <ExternalLink
+        href="https://www.cnil.fr/fr/modele/courrier/rectifier-des-donnees-inexactes-obsoletes-ou-perimees"
+        text="un modèle de courrier élaboré par la Cnil"
+      />
+      est à votre disposition.
+    </p>
+    <p>
+      Le responsable de traitement s’engage à répondre dans un délai raisonnable
+      qui ne saurait dépasser 1 mois à compter de la réception de votre demande.
+    </p>
+
+    <h2>Destinataires des données</h2>
+    <p>
+      Le responsable de traitement s’engage à ce que les données soient traitées
+      par les seules personnes autorisées.
+    </p>
+    <p>
+      Ces destinaires sont : les agents DSF, les agents habilités à rechercher
+      les infractions relatives aux signalisations des symptomes en forêt.
+    </p>
+    <h3>Sous-traitants</h3>
+    <p>
+      Certaines des données sont envoyées à des sous-traitants pour réaliser
+      certaines missions. Le responsable de traitement s'est assuré de la mise
+      en œuvre par ses sous-traitants de garanties adéquates et du respect de
+      conditions strictes de confidentialité, d’usage et de protection des
+      données.
+    </p>
+    <DsfrTable
+      title="Sous-traitants"
+      :rows="[
+        [
+          'Clever Cloud',
+          'France',
+          'Hébergement',
+          'https://www.clever-cloud.com/fr/privacy-policy',
+        ],
+      ]"
+      no-caption
+      :pagination="false"
+    />
+  </div>
+</template>
