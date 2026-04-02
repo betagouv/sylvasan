12c8d4ab116a46b8b6dd14efa6b314733b401b38
Alejandro MG <alejandro@amguillen.dev>
2026-04-02 10:31:30 +0200

    Enlève import inutilisé

--- web/src/components/SurveyBuilder/NewFieldModal.vue
+++ web/src/components/SurveyBuilder/NewFieldModal.vue
@@ -1,5 +1,5 @@
 <script setup lang="ts">
-import { ref, computed } from "vue"
+import { ref } from "vue"
 import type { SurveyField } from "@shared-types/survey"
 import * as z from "zod"
 import { ZodError } from "zod"
