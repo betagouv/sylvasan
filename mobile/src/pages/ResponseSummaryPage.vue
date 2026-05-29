7c73bc1d7b83ace89d42a2ad220aa7ca66703339
Alejandro MG <alejandro@amguillen.dev>
2026-05-29 15:55:40 +0200

    Permet de supprimer une observation non sauvegardée dans le backend

--- mobile/src/pages/ResponseSummaryPage.vue
+++ mobile/src/pages/ResponseSummaryPage.vue
@@ -7,13 +7,19 @@ import {
   IonContent,
   IonButtons,
   IonBackButton,
+  IonButton,
+  IonIcon,
+  useIonRouter,
+  alertController,
 } from "@ionic/vue"
+import { trashOutline } from "ionicons/icons"
 import { useRoute } from "vue-router"
 import { useResponsesStore } from "../stores/responses"
 import { useSurveysStore } from "../stores/surveys"
 import SurveySummary from "../components/SurveySummary.vue"
 
 const route = useRoute()
+const router = useIonRouter()
 const responsesStore = useResponsesStore()
 const surveysStore = useSurveysStore()
 
@@ -39,6 +45,34 @@ const survey = computed(() =>
     ? surveysStore.getSurveyById(surveyId.value)
     : undefined
 )
+
+const isDeletable = computed(
+  () =>
+    response.value != null &&
+    "localId" in response.value &&
+    response.value.status !== "synced"
+)
+
+const confirmDelete = async () => {
+  if (!response.value || !("localId" in response.value)) return
+  const localId = response.value.localId
+  const alert = await alertController.create({
+    header: "Supprimer cette observation ?",
+    message: "Cette observation sera définitivement supprimée.",
+    buttons: [
+      { text: "Annuler", role: "cancel" },
+      {
+        text: "Supprimer",
+        role: "destructive",
+        handler: async () => {
+          await responsesStore.deleteDraft(localId)
+          router.navigate({ name: "ResponseListPage" }, "back", "replace")
+        },
+      },
+    ],
+  })
+  await alert.present()
+}
 </script>
 
 <template>
@@ -47,6 +81,16 @@ const survey = computed(() =>
       <ion-toolbar>
         <ion-buttons slot="start">
           <ion-back-button :default-href="{ name: 'ResponseListPage' }" />
+        </ion-buttons>
+        <ion-buttons slot="end">
+          <ion-button
+            v-if="isDeletable"
+            color="danger"
+            class="pr-2"
+            @click="confirmDelete"
+          >
+            <ion-icon slot="icon-only" :icon="trashOutline" />
+          </ion-button>
         </ion-buttons>
       </ion-toolbar>
     </ion-header>
