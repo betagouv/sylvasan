f1343a6e2f12b14e7e2ef04c04499fc372f15746
Alejandro MG <alejandro@amguillen.dev>
2026-06-19 14:12:34 +0200

    Ajout des margins et des titres dans les headers

--- mobile/src/pages/ProfilePage.vue
+++ mobile/src/pages/ProfilePage.vue
@@ -29,11 +29,11 @@ const logout = async () => {
 <template>
   <ion-page>
     <ion-header>
-      <ion-toolbar />
+      <ion-toolbar>
+        <ion-title>Mon profil</ion-title>
+      </ion-toolbar>
     </ion-header>
     <ion-content class="ion-padding">
-      <h1 class="fr-h3">Mon profil</h1>
-
       <DsfrTag
         icon="ri-links-line"
         v-if="loggedUser?.source !== 'local'"
@@ -42,9 +42,12 @@ const logout = async () => {
       />
 
       <section class="fr-mb-6w">
-        <h2 class="fr-h5">Informations personnelles</h2>
         <dl class="pl-0!">
-          <div v-for="info in personalInformation" :key="info.key" class="fr-mb-2w">
+          <div
+            v-for="info in personalInformation"
+            :key="info.key"
+            class="fr-mb-2w"
+          >
             <dt class="font-bold fr-text--sm mb-0!">{{ info.key }}</dt>
             <dd class="ml-0! pl-0!">{{ info.value }}</dd>
           </div>
@@ -53,7 +56,10 @@ const logout = async () => {
 
       <section class="fr-mb-6w">
         <h2 class="fr-h5">Rôles</h2>
-        <p v-if="!loggedUser?.memberships.length" class="fr-text--sm text-gray-500">
+        <p
+          v-if="!loggedUser?.memberships.length"
+          class="fr-text--sm text-gray-500"
+        >
           Aucun rôle assigné.
         </p>
         <div v-else class="flex flex-col gap-3">
@@ -63,9 +69,13 @@ const logout = async () => {
             class="fr-p-3w border border-gray-200 rounded"
           >
             <p class="font-bold fr-text--sm mb-1!">{{ m.organisation.name }}</p>
-            <p class="fr-text--sm mb-1!">{{ m.pole?.name ?? "Tous les pôles" }}</p>
+            <p class="fr-text--sm mb-1!">
+              {{ m.pole?.name ?? "Tous les pôles" }}
+            </p>
             <DsfrBadge
-              :label="membershipTypeLabels[m.membershipType] ?? m.membershipType"
+              :label="
+                membershipTypeLabels[m.membershipType] ?? m.membershipType
+              "
               type="info"
               no-icon
             />
