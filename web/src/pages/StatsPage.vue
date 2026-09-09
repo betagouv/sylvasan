e43a39960db51477d715bf8655300d3fcfb8ae61
Alejandro MG <alejandro@amguillen.dev>
2026-09-09 11:30:39 +0200

    Connexion entre front et back

--- web/src/pages/StatsPage.vue
+++ web/src/pages/StatsPage.vue
@@ -8,7 +8,17 @@
 }
 </route>
 
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { useApiFetch } from "../utils/data-fetching.ts"
+import ProgressSpinner from "../components/ProgressSpinner.vue"
+
+const { data, isFetching } = useApiFetch("/stats").get().json()
+
+const chartTag = (type: string) => `${type}-chart`
+const chartX = (stat: any) => JSON.stringify([stat.data.labels])
+const chartY = (stat: any) => JSON.stringify(stat.data.datasets.map((d: any) => d.data))
+const chartName = (stat: any) => JSON.stringify(stat.data.datasets.map((d: any) => d.label))
+</script>
 
 <template>
   <div class="fr-container my-10">
@@ -17,10 +27,22 @@
     />
     <h1>Mesures d'impact</h1>
 
-    <bar-chart
-      x='[["2025", "2030", "2035", "2040"]]'
-      y="[[10, 20, 30, 40]]"
-      selected-palette="categorical"
-    ></bar-chart>
+    <div v-if="isFetching" class="flex justify-center my-20">
+      <ProgressSpinner />
+    </div>
+
+    <div v-else class="flex flex-col gap-10">
+      <div v-for="stat in data" :key="stat.id">
+        <h2>{{ stat.title }}</h2>
+        <p class="fr-text--lead">{{ stat.description }}</p>
+        <component
+          :is="chartTag(stat.type)"
+          :x="chartX(stat)"
+          :y="chartY(stat)"
+          :name="chartName(stat)"
+          selected-palette="categorical"
+        />
+      </div>
+    </div>
   </div>
 </template>
