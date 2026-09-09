8229757b236ccbeac791feba086c02b193e05e6a
Alejandro MG <alejandro@amguillen.dev>
2026-09-09 11:52:54 +0200

    Ajout du bandeau bleu

--- web/src/pages/StatsPage.vue
+++ web/src/pages/StatsPage.vue
@@ -20,7 +20,11 @@ const flatX = (stat: any) => JSON.stringify(stat.data.labels)
 const yLine = (stat: any) => JSON.stringify(stat.data.datasets[0].data)
 const yBar = (stat: any) => {
   const cumulative: number[] = stat.data.datasets[0].data
-  return JSON.stringify(cumulative.map((v: number, i: number) => (i === 0 ? v : v - cumulative[i - 1])))
+  return JSON.stringify(
+    cumulative.map((v: number, i: number) =>
+      i === 0 ? v : v - cumulative[i - 1]
+    )
+  )
 }
 const nameLine = (stat: any) => stat.data.datasets[0].label
 const yMax = (stat: any) => {
@@ -30,35 +34,40 @@ const yMax = (stat: any) => {
 </script>
 
 <template>
-  <div class="fr-container my-10">
-    <DsfrBreadcrumb
-      :links="[{ to: '/', text: 'Accueil' }, { text: 'Mesures d\'impact' }]"
-    />
-    <h1>Mesures d'impact</h1>
-
-    <div v-if="isFetching" class="flex justify-center my-20">
-      <ProgressSpinner />
-    </div>
-
-    <DsfrAccordionsGroup v-else v-model="activeAccordion">
-      <DsfrAccordion
-        v-for="stat in data"
-        :key="stat.id"
-        :id="stat.id"
-        :title="stat.title"
-      >
-        <p class="fr-text--lead">{{ stat.description }}</p>
-        <bar-line-chart
-          :x="flatX(stat)"
-          :y-bar="yBar(stat)"
-          :y-line="yLine(stat)"
-          name-bar="Par mois"
-          :name-line="nameLine(stat)"
-          :y-bar-max="yMax(stat)"
-          :y-line-max="yMax(stat)"
-          selected-palette="categorical"
+  <div>
+    <div class="bg-blue-france-975 py-1">
+      <div class="fr-container">
+        <DsfrBreadcrumb
+          :links="[{ to: '/', text: 'Accueil' }, { text: 'Mesures d\'impact' }]"
         />
-      </DsfrAccordion>
-    </DsfrAccordionsGroup>
+        <h1>Mesures d'impact</h1>
+      </div>
+    </div>
+    <div class="fr-container my-10">
+      <div v-if="isFetching" class="flex justify-center my-20">
+        <ProgressSpinner />
+      </div>
+
+      <DsfrAccordionsGroup v-else v-model="activeAccordion">
+        <DsfrAccordion
+          v-for="stat in data"
+          :key="stat.id"
+          :id="stat.id"
+          :title="stat.title"
+        >
+          <p class="fr-text--lead">{{ stat.description }}</p>
+          <bar-line-chart
+            :x="flatX(stat)"
+            :y-bar="yBar(stat)"
+            :y-line="yLine(stat)"
+            name-bar="Par mois"
+            :name-line="nameLine(stat)"
+            :y-bar-max="yMax(stat)"
+            :y-line-max="yMax(stat)"
+            selected-palette="categorical"
+          />
+        </DsfrAccordion>
+      </DsfrAccordionsGroup>
+    </div>
   </div>
 </template>
